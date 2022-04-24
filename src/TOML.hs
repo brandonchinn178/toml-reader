ce8fb368c36583e660e878537fab7822fde554a6
Brandon Chinn <brandonchinn178@gmail.com>
2022-04-23 20:07:23 -0700

    Implement validator

--- src/TOML.hs
+++ src/TOML.hs
@@ -1,13 +1,29 @@
 module TOML (
   decode,
-  -- TODO
+  Value (..),
 ) where
 
+import Data.Map (Map)
 import Data.Text (Text)
+import Data.Time (Day, LocalTime, TimeOfDay, UTCTime)
+
+data Value
+  = Table (Map Text Value)
+  | Array [Value]
+  | String Text
+  | Integer Integer
+  | Float Double
+  | Boolean Bool
+  | OffsetDateTime UTCTime
+  | LocalDateTime LocalTime
+  | LocalDate Day
+  | LocalTime TimeOfDay
+  deriving (Show, Eq)
 
 decode :: FromTOML a => Text -> Either TOMLError a
 decode = undefined
 
 -- TODO
-data TOMLError
+data TOMLError = TOMLError deriving (Show)
 class FromTOML a
+instance FromTOML Value
--- test/Validate.hs
+++ test/Validate.hs
@@ -1,25 +1,92 @@
+{-# LANGUAGE CPP #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
 
+import Data.Aeson ((.=))
+import qualified Data.Aeson as Aeson
+import qualified Data.ByteString.Lazy.Char8 as Char8
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.Text (Text)
 import qualified Data.Text.IO as Text
+import qualified Data.Vector as Vector
+import System.Directory (findExecutable)
 import System.Environment (getArgs, getExecutablePath)
+import System.Process (callProcess)
+
+import TOML (Value (..), decode)
+
+#if MIN_VERSION_aeson(2,0,0)
+import qualified Data.Aeson.Key as Key
+import Data.Aeson.KeyMap (KeyMap)
+import qualified Data.Aeson.KeyMap as KeyMap
+#else
+import qualified Data.HashMap.Lazy as HashMap
+#endif
+
+#if MIN_VERSION_time(1,9,0)
+import Data.Time.Format.ISO8601 (iso8601Show)
+#else
+import Data.Time (Day, LocalTime, TimeOfDay, UTCTime)
+import Data.Time.Format (defaultTimeLocale, formatTime, iso8601DateFormat)
+#endif
 
 main :: IO ()
 main =
   getArgs >>= \case
-    [] -> runTomlTest
     ["--check"] -> checkTOML
-    args -> error $ "Invalid arguments: " ++ show args
+    args -> runTomlTest args
 
 -- | Run 'toml-test -- $0 --check'
-runTomlTest :: IO ()
-runTomlTest = do
-  exe <- getExecutablePath
-  -- TODO
-  print exe
+runTomlTest :: [String] -> IO ()
+runTomlTest args = do
+  thisExe <- getExecutablePath
+  findExecutable "toml-test" >>= \case
+    Nothing -> error "Could not find 'toml-test' executable"
+    Just tomlTestExe ->
+      callProcess tomlTestExe $
+        args ++ ["-color", "always", "--", thisExe, "--check"]
 
 -- | Parse TOML data in stdin.
 checkTOML :: IO ()
 checkTOML = do
   input <- Text.getContents
-  -- TODO
-  print input
+  output <- either (error . show) return $ decode input
+  Char8.putStrLn $ Aeson.encode $ toTaggedJSON output
+
+toTaggedJSON :: Value -> Aeson.Value
+toTaggedJSON = \case
+  Table o -> Aeson.Object $ toKeyMap $ toTaggedJSON <$> o
+  Array vs -> Aeson.Array $ Vector.fromList $ map toTaggedJSON vs
+  String x -> tagged "string" x
+  Integer x -> tagged "integer" x
+  Float x -> tagged "float" x
+  Boolean x -> tagged "bool" x
+  OffsetDateTime x -> tagged "datetime" $ iso8601Show x
+  LocalDateTime x -> tagged "datetime-local" $ iso8601Show x
+  LocalDate x -> tagged "date-local" $ iso8601Show x
+  LocalTime x -> tagged "time-local" $ iso8601Show x
+  where
+    tagged :: Aeson.ToJSON a => String -> a -> Aeson.Value
+    tagged ty v = Aeson.object ["type" .= ty, "value" .= v]
+
+#if MIN_VERSION_aeson(2,0,0)
+toKeyMap :: Map Text Aeson.Value -> KeyMap Aeson.Value
+toKeyMap = KeyMap.fromMap . Map.mapKeys Key.fromText
+#else
+toKeyMap :: Map Text Aeson.Value -> Aeson.Object
+toKeyMap = HashMap.fromList . Map.toList
+#endif
+
+#if !MIN_VERSION_time(1,9,0)
+class ISO8601 a where
+  iso8601Show :: a -> String
+instance ISO8601 UTCTime where
+  iso8601Show = formatTime defaultTimeLocale (iso8601DateFormat $ Just "%T%Q%Z")
+instance ISO8601 LocalTime where
+  iso8601Show = formatTime defaultTimeLocale (iso8601DateFormat $ Just "%T%Q")
+instance ISO8601 Day where
+  iso8601Show = formatTime defaultTimeLocale (iso8601DateFormat Nothing)
+instance ISO8601 TimeOfDay where
+  iso8601Show = formatTime defaultTimeLocale "%T%Q"
+#endif
