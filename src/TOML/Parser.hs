7a9a9402ae1c7f58f5882071c36585d0c1cc51e3
Brandon Chinn <brandonchinn178@gmail.com>
2022-04-24 00:37:11 -0700

    Implement parseKey

--- src/TOML/Parser.hs
+++ src/TOML/Parser.hs
@@ -1,18 +1,28 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
 
+{- |
+Parse a TOML document.
+
+References:
+* https://toml.io/en/v1.0.0
+* https://github.com/toml-lang/toml/blob/1.0.0/toml.abnf
+-}
 module TOML.Parser (
   parseTOML,
 ) where
 
+import Data.Char (ord)
+import Data.Map (Map)
 import qualified Data.Map as Map
 import Data.Text (Text)
+import qualified Data.Text as Text
 import Data.Void (Void)
 import Text.Megaparsec
-import Text.Megaparsec.Char hiding (newline, space)
+import Text.Megaparsec.Char hiding (newline)
 import qualified Text.Megaparsec.Char.Lexer as L
 
-import TOML.Internal (TOMLError (..), Table, Value (..))
+import TOML.Internal (TOMLError (..), Value (..))
 
 parseTOML :: String -> Text -> Either TOMLError Value
 parseTOML filename input =
@@ -24,6 +34,9 @@ parseTOML filename input =
 
 type Parser = Parsec Void Text
 
+type Key = [Text]
+type Table = Map Key Value
+
 data TOMLDoc = TOMLDoc
   { rootTable :: Table
   , subTables :: [TableSection]
@@ -34,15 +47,15 @@ data TableSection = TableSection
   , tableSectionTable :: Table
   }
 
-data TableSectionHeader = SectionTable Text | SectionTableArray Text
+data TableSectionHeader = SectionTable Key | SectionTableArray Key
 
 parseTOMLDocument :: Parser TOMLDoc
 parseTOMLDocument = do
-  space
+  emptyLines
   rootTable <- parseTable
-  space
+  emptyLines
   subTables <- many parseTableSection
-  space
+  emptyLines
   eof
   return TOMLDoc{..}
 
@@ -55,7 +68,7 @@ parseTable =
     hspace
     value <- parseValue
     newline
-    space
+    emptyLines
     return (key, value)
 
 parseTableSection :: Parser TableSection
@@ -66,9 +79,9 @@ parseTableSection = do
       , SectionTable <$> parseHeader "[" "]"
       ]
   newline
-  space
+  emptyLines
   tableSectionTable <- parseTable
-  space
+  emptyLines
   return TableSection{..}
   where
     parseHeader brackStart brackEnd = do
@@ -79,17 +92,53 @@ parseTableSection = do
       _ <- string brackEnd
       return key
 
-parseKey :: Parser Text
-parseKey = undefined
+parseKey :: Parser Key
+parseKey =
+  fmap (map Text.pack) $
+    (`sepBy` hsymbol ".") . choice $
+      [ between (hsymbol "\"") (hsymbol "\"") (many $ satisfy isBasicChar)
+      , between (hsymbol "'") (hsymbol "'") (many $ satisfy isLiteralChar)
+      , unquotedString
+      ]
+  where
+    isBasicChar c =
+      let code = ord c
+       in case c of
+            ' ' -> True
+            '\t' -> True
+            _ | 0x21 <= code && code <= 0x7E -> c `notElem` ['"', '\\']
+            _ | isNonAscii c -> True
+            -- TODO: escape sequences
+            -- \", \\, \b, \f, \n, \r, \t, \uXXXX, \UXXXXXXXX
+            _ -> False
+    isLiteralChar c =
+      let code = ord c
+       in case c of
+            ' ' -> True
+            '\t' -> True
+            _ | 0x21 <= code && code <= 0x7E -> c /= '\''
+            _ | isNonAscii c -> True
+            _ -> False
+    unquotedString =
+      some $
+        label "[A-Za-z0-9_-]" . oneOf $
+          ['A' .. 'Z'] ++ ['a' .. 'z'] ++ ['0' .. '9'] ++ "-_"
+    isNonAscii c =
+      let code = ord c
+       in (0x80 <= code && code <= 0xD7FF) || (0xE000 <= code && code <= 0x10FFFF)
 
 parseValue :: Parser Value
 parseValue = undefined
 
 {--- Helpers ---}
 
+hsymbol :: Text -> Parser ()
+hsymbol s = L.symbol hspace s >> pure ()
+
 -- | Parse trailing whitespace + newline
 newline :: Parser ()
 newline = hspace >> eol >> pure ()
 
-space :: Parser ()
-space = L.space space1 (L.skipLineComment "#") empty
+-- | Parse spaces, newlines, and comments
+emptyLines :: Parser ()
+emptyLines = L.space space1 (L.skipLineComment "#") empty
