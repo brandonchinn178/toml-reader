e9fc54a8e35dda7ab72092338363eb4d3022884b
Brandon Chinn <brandonchinn178@gmail.com>
2022-04-23 23:37:38 -0700

    Parse table sections

--- src/TOML/Parser.hs
+++ src/TOML/Parser.hs
@@ -9,7 +9,7 @@ import qualified Data.Map as Map
 import Data.Text (Text)
 import Data.Void (Void)
 import Text.Megaparsec
-import Text.Megaparsec.Char hiding (space)
+import Text.Megaparsec.Char hiding (newline, space)
 import qualified Text.Megaparsec.Char.Lexer as L
 
 import TOML.Internal (TOMLError (..), Table, Value (..))
@@ -29,31 +29,55 @@ data TOMLDoc = TOMLDoc
   , subTables :: [TableSection]
   }
 
-data TableSection
-  = TableSection Text Table
-  | TableArraySection Text Table
+data TableSection = TableSection
+  { tableSectionHeader :: TableSectionHeader
+  , tableSectionTable :: Table
+  }
+
+data TableSectionHeader = SectionTable Text | SectionTableArray Text
 
 parseTOMLDocument :: Parser TOMLDoc
 parseTOMLDocument = do
+  space
   rootTable <- parseTable
+  space
   subTables <- many parseTableSection
+  space
   eof
   return TOMLDoc{..}
 
 parseTable :: Parser Table
 parseTable =
   fmap Map.fromList . many $ do
-    space
     key <- parseKey
     hspace
     _ <- string "="
     hspace
     value <- parseValue
+    newline
     space
     return (key, value)
 
 parseTableSection :: Parser TableSection
-parseTableSection = undefined
+parseTableSection = do
+  tableSectionHeader <-
+    choice
+      [ SectionTableArray <$> parseHeader "[[" "]]"
+      , SectionTable <$> parseHeader "[" "]"
+      ]
+  newline
+  space
+  tableSectionTable <- parseTable
+  space
+  return TableSection{..}
+  where
+    parseHeader brackStart brackEnd = do
+      _ <- string brackStart
+      hspace
+      key <- parseKey
+      hspace
+      _ <- string brackEnd
+      return key
 
 parseKey :: Parser Text
 parseKey = undefined
@@ -62,6 +86,10 @@ parseValue :: Parser Value
 parseValue = undefined
 
 {--- Helpers ---}
+
+-- | Parse trailing whitespace + newline
+newline :: Parser ()
+newline = hspace >> eol >> pure ()
 
 space :: Parser ()
 space = L.space space1 (L.skipLineComment "#") empty
