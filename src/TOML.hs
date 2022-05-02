8a8d936a21982db0ca8636d95b54e6ec04886540
Brandon Chinn <brandonchinn178@gmail.com>
2022-05-01 19:57:55 -0700

    Break out TOML.Decode

--- src/TOML.hs
+++ src/TOML.hs
@@ -1,37 +1,15 @@
 module TOML (
+  -- * Decoding a TOML file
   decode,
   decodeFile,
+  FromTOML (..),
+  Decoder,
+
+  -- * TOML types
   Value (..),
   Table,
-  FromTOML (..),
   TOMLError (..),
 ) where
 
-import Data.Text (Text)
-import qualified Data.Text.IO as Text
-
-import TOML.Internal (TOMLError (..), Table, Value (..))
-import TOML.Parser (parseTOML)
-
-{--- FromTOML ---}
-
--- TODO: do a similar thing to jordan to parse and deserialize at the same time
--- https://github.com/AnthonySuper/jordan
-newtype Parser a = Parser {unParser :: Value -> Either TOMLError a}
-
-class FromTOML a where
-  fromTOML :: Parser a
-
-instance FromTOML Value where
-  fromTOML = Parser Right
-
-{--- Decoding ---}
-
-decode :: FromTOML a => Text -> Either TOMLError a
-decode = decodeWithFilename ""
-
-decodeFile :: FromTOML a => FilePath -> IO (Either TOMLError a)
-decodeFile fp = decodeWithFilename fp <$> Text.readFile fp
-
-decodeWithFilename :: FromTOML a => String -> Text -> Either TOMLError a
-decodeWithFilename filename input = parseTOML filename input >>= unParser fromTOML
+import TOML.Decode
+import TOML.Internal
